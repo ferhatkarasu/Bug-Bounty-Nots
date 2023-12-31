ae9aef184bd2524b3901fd2ed8232a29186b225b
Ferhat Karasu <74055034+ferhatkarasu@users.noreply.github.com>
2023-12-31 16:21:50 +0300

    Update openRecirect.sh

--- 000-🗲myTools🗲/openRecirect.sh
+++ 000-🗲myTools🗲/openRecirect.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
 
+#Kullanım -> ./openRecirect.sh result.txt
+#!/bin/bash
 grep -E "next=|url=|target=|rurl=|dest=|destination=|redir=|redirect_uri=|redirect_url=|redirect=|redirection_url=|/redirect/|/cgi-bin/redirect.cgi|/out/|/out|view=|/loginto=|image_url=|go=|return=|returnTo=|return_to=|checkout_url=|continue=|return_path=" $1
