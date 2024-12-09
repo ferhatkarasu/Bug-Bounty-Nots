be26f694afee909aac4e3489626df11c5c923c9b
Ferhat Karasu <74055034+ferhatkarasu@users.noreply.github.com>
2024-12-09 10:51:25 +0300

    Delete 000-🗲myTools🗲 directory

--- 000-🗲myTools🗲/openRecirect.sh
+++
@@ -1,4 +0,0 @@
-
-#Kullanım -> ./openRecirect.sh result.txt
-#!/bin/bash
-grep -E "next=|url=|target=|rurl=|dest=|destination=|redir=|redirect_uri=|redirect_url=|redirect=|redirection_url=|/redirect/|/cgi-bin/redirect.cgi|/out/|/out|view=|/loginto=|image_url=|go=|return=|returnTo=|return_to=|checkout_url=|continue=|return_path=" $1
